2f7827bff7042d08a33ffc359da88e3c52054706
ramteke3yash <ramteke.yashkun@gmail.com>
2023-07-30 11:54:41 +0530

    learning how to create a server on nodejs

--- first-app.js
+++ first-app.js
@@ -81,3 +81,42 @@
 
 // console.log(obj1);
 // console.log(obj2);
+
+// const obj1 = { key1: 1, key2: 2, key3: 1000 };
+// const { key1, key3 } = { ...obj1 };
+// console.log(key1, key3);
+// const arr1 = ["value1", "value2"];
+// const [val1, val2] = arr1;
+// console.log(val1);
+
+// console.log(val2);
+
+// const obj1 = { key1: 1, key2: 2, key3: 1000 };
+// let { key1, key3 } = obj1;
+// key1 = 20;
+// key3 = 123;
+// console.log(obj1.key1, obj1.key3);
+// console.log("a");
+// console.log("b");
+// setTimeout(() => console.log("c"), 3000);
+// console.log("d");
+
+// console.log("a");
+// console.log("b");
+// setTimeout(() => console.log("c"), 3000);
+// setTimeout(() => console.log("d"), 0);
+// console.log("e");
+
+// function delay(time) {
+//   return new Promise((resolve) => setTimeout(resolve, time));
+// }
+// async function printSequence() {
+//   console.log("a");
+//   console.log("b");
+//   await delay(3000);
+//   console.log("c");
+//   await delay(0);
+//   console.log("d");
+//   console.log("e");
+// }
+// printSequence();
