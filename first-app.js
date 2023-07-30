429b56de2d074221e78497ab655c42d3ec9a7c40
ramteke3yash <ramteke.yashkun@gmail.com>
2023-07-30 06:08:40 +0530

    revising fat arrow functions and object

--- first-app.js
+++ first-app.js
@@ -1,4 +1,22 @@
-const fs = require("fs");
+// const fs = require("fs");
 
-fs.writeFileSync("hello.txt", "Hello from Node.js");
-g;
+// fs.writeFileSync("hello.txt", "Hello from Node.js");
+
+//   ------fat arrow funtions
+const add = (a, b) => a + b;
+console.log(add(1, 2));
+
+// const addOne = (a) => a + 1;
+// console.log(addOne(1));
+
+// const addRandom = () => 6 + 4;
+// console.log(addRandom());
+
+const person = {
+  name: "Yash",
+  age: 23,
+  greet() {
+    console.log("Hi, I am " + this.name);
+  },
+};
+person.greet();
