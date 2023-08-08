8e9629325fdf494cc8d82e530ad5302a49396003
ramteke3yash <ramteke.yashkun@gmail.com>
2023-08-08 20:10:38 +0530

    learning routes and filters

--- server_app.js
+++ server_app.js
@@ -3,21 +3,16 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/add-products", (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button></form>'
-  );
-});
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
 
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello from express!</h1>");
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
 });
 
 app.listen(3000);
