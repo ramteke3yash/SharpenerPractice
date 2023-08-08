f90609685c74ddd45859c5d542a20ba90a338f10
ramteke3yash <ramteke.yashkun@gmail.com>
2023-08-08 16:40:10 +0530

    learning express

--- server_app.js
+++ server_app.js
@@ -1,8 +1,17 @@
-const http = require("http");
+const express = require("express");
 
-const routes = require("./routes");
-//console.log(routes.someText);
+const app = express();
 
-const server = http.createServer(routes);
+app.use((req, res, next) => {
+  console.log("in the middleware");
+  next(); //allows the request to continue to the next middleware
+});
 
-server.listen(3000);
+app.use((req, res, next) => {
+  console.log("in another middleware");
+  res.send("<h1>Hello from express!</h1>");
+});
+
+app.listen(3000);
+// const server = http.createServer(app);
+// server.listen(3000);
